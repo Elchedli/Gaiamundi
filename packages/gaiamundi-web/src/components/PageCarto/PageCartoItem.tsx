f43217e2d7b1321a8b28c9ebe6f007aef8480582
elchedli <shidonosan@gmail.com>
2022-12-20 15:07:00 +0100

    fix: added imageplaceholder for null images

--- packages/gaiamundi-web/src/components/PageCarto/PageCartoItem.tsx
+++ packages/gaiamundi-web/src/components/PageCarto/PageCartoItem.tsx
@@ -10,23 +10,29 @@ const PageCartoItem: React.FC<PageCarto> = ({
   return (
     <Link to={`/page-carto/${id}?populate=*`} className="group border">
       <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden bg-gray-400 xl:aspect-w-7 xl:aspect-h-8">
-        <img
-          src={'http://localhost:1337' + ImgUrl}
-          className="h-48 w-full object-cover object-center group-hover:opacity-75"
-        />
+        {
+          <img
+            src={
+              ImgUrl
+                ? 'http://localhost:1337' + ImgUrl
+                : process.env.PUBLIC_URL + '/imageplaceholder.png'
+            }
+            className="h-48 w-full object-cover object-center group-hover:opacity-75"
+          />
+        }
       </div>
       <div className="p-2">
         <h3 className="text-lg font-medium text-gray-900">Titre: {name}</h3>
-        <h3 className="text-lg font-medium text-gray-900">
-          Titre Map: {(map as any)?.data?.attributes?.name}
-        </h3>
         <h4 className="text-xs text-gray-500">
-          Créer par: {owner?.data?.attributes?.username}
+          Nom Map : {(map as any)?.data?.attributes?.name}
+        </h4>
+        <h4 className="text-xs text-gray-500">
+          Créer par : {owner?.data?.attributes?.username}
         </h4>
         <h4 className="text-xs text-gray-500">
           Tags :
           {(tags as any)?.data?.map((tag: any) => {
-            return tag?.attributes?.name;
+            return tag?.attributes?.name + ' | ';
           })}
         </h4>
         <h4 className="text-xs text-gray-500"></h4>
