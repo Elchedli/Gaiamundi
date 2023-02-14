bdae5e22698403167b90505986968af8c768dbeb
elchedli <shidonosan@gmail.com>
2023-02-14 16:57:24 +0100

    fix: add describe in label.test.tsx

--- packages/gaiamundi-web/src/components/Forms/Inputs/__test__/Label.test.tsx
+++ packages/gaiamundi-web/src/components/Forms/Inputs/__test__/Label.test.tsx
@@ -1,9 +1,12 @@
 import { render } from '@testing-library/react';
 import { Label } from '../Label';
-it('renders color correctly and children classname', () => {
-  const color = 'red';
-  const { container } = render(<Label color={color} className="text-lg" />);
-  const label = container.firstChild;
-  expect(label).toHaveClass(`text-red-700`);
-  expect(label).toHaveClass(`text-lg`);
+
+describe('Label', () => {
+  it('renders color correctly and children classname', () => {
+    const color = 'red';
+    const { container } = render(<Label color={color} className="text-lg" />);
+    const label = container.firstChild;
+    expect(label).toHaveClass(`text-red-700`);
+    expect(label).toHaveClass(`text-lg`);
+  });
 });
