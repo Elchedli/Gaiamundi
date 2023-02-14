4088730c930b0e53900b6b48e46b88b1f3158a3d
elchedli <shidonosan@gmail.com>
2023-02-14 16:29:26 +0100

    test: listboxinput unit testing

---
+++ packages/gaiamundi-web/src/components/Forms/Inputs/__test__/ListBoxInput.test.tsx
@@ -0,0 +1,54 @@
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import { ListBoxInput } from '../ListBoxInput';
+
+afterEach(cleanup);
+
+describe('ListBoxInput', () => {
+  const options = [
+    { value: 1, label: 'Option 1' },
+    { value: 2, label: 'Option 2' },
+    { value: 3, label: 'Option 3' },
+  ];
+
+  it('renders the options list when button is clicked', () => {
+    const onChange = jest.fn();
+    const { getByRole, getByTestId } = render(
+      <ListBoxInput defaultValue={1} options={options} onChange={onChange} />
+    );
+
+    fireEvent.click(getByRole('button'));
+    expect(getByTestId('Option 1')).toBeInTheDocument();
+    expect(getByTestId('Option 2')).toBeInTheDocument();
+    expect(getByTestId('Option 3')).toBeInTheDocument();
+  });
+
+  it('updates the selected value when an option is clicked', () => {
+    const handleChange = jest.fn();
+    const { getByText, getByRole } = render(
+      <ListBoxInput
+        defaultValue={1}
+        options={options}
+        onChange={handleChange}
+      />
+    );
+
+    fireEvent.click(getByRole('button'));
+    fireEvent.click(getByText('Option 2'));
+
+    expect(handleChange).toHaveBeenCalledWith(2);
+  });
+
+  it('displays the selected label on the button', () => {
+    const onChange = jest.fn();
+    const { getByText, getByRole } = render(
+      <ListBoxInput defaultValue={1} options={options} onChange={onChange} />
+    );
+
+    expect(getByRole('button')).toHaveTextContent('Option 1');
+
+    fireEvent.click(getByRole('button'));
+    fireEvent.click(getByText('Option 2'));
+
+    expect(getByRole('button')).toHaveTextContent('Option 2');
+  });
+});
