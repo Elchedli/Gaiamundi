16d89995d61ad564a99415036b1924bd50acced5
elchedli <shidonosan@gmail.com>
2023-03-10 12:10:14 +0100

    test: csvuploader added comments and small fix

--- packages/gaiamundi-web/src/components/PageCarto/Editor/PageCartoDataPanel/__test__/CsvUploader.test.tsx
+++ packages/gaiamundi-web/src/components/PageCarto/Editor/PageCartoDataPanel/__test__/CsvUploader.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { useAuth } from 'hooks/useAuth';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -28,9 +28,9 @@ describe('CsvUploader', () => {
   const onCancelMock = jest.fn();
   const onChangeMock = jest.fn();
 
-  it('should upload a CSV file and click  on cancel button when upload is successful', async () => {
+  it('should upload a CSV file and click on cancel button when upload is successful', async () => {
     const file = new File(['a,b,c\n1,2,3'], 'file.csv', { type: 'text/csv' });
-    const { container } = render(
+    const { container, getByTestId, findByTestId } = render(
       <QueryClientProvider client={queryClient}>
         <CsvUploader
           onUpload={onUploadMock}
@@ -40,11 +40,16 @@ describe('CsvUploader', () => {
         />
       </QueryClientProvider>
     );
-    const fileInput = await screen.findByTestId('hidden-file-input');
+    const fileInput = getByTestId('hidden-file-input');
+    expect(fileInput).toBeInTheDocument();
+    //upload file so we can open the mini page that contains a name and a red cancel button
     userEvent.upload(fileInput, file);
-    const loadedFilePage = await screen.findByTestId('loadedFilePage');
+    // wait for the mini page when the upload and criteria are successful
+    const loadedFilePage = await findByTestId('file-loaded-minipage');
     expect(loadedFilePage).toBeInTheDocument();
+    //click on the cancel button to get back to the upload mini page "DropZone"
     userEvent.click(loadedFilePage.querySelector('button') as HTMLElement);
+    //check if the mini page is removed
     expect(container.contains(loadedFilePage)).not.toBeTruthy();
   });
 });
