3886db36d80534d40b110683c8cba50f67b1fcd9
AndyMatloub <andy.matloub@hotmail.com>
2023-03-15 11:52:28 +0100

    test: update test of FilterBar component

--- packages/gaiamundi-web/src/components/PageCarto/Dashboard/__test__/FilterBar.test.tsx
+++ packages/gaiamundi-web/src/components/PageCarto/Dashboard/__test__/FilterBar.test.tsx
@@ -1,13 +1,16 @@
 import { fireEvent, render } from '@testing-library/react';
 import { FilterBar } from 'components/PageCarto/Dashboard/FilterBar';
-import { mockTags } from 'utils/mocks/data';
+import { useAuth } from 'hooks/useAuth';
+import { useQuery } from 'react-query';
+import { mockUser } from 'utils/mocks/data';
+import { Dashboard } from '../Dashboard';
 
 const props = {
   onSearchKeywordChange: jest.fn(),
   onTagChange: jest.fn(),
-  tags: mockTags,
 };
 
+jest.mock('hooks/useAuth');
 describe('FilterBar', () => {
   test('renders without error', () => {
     render(<FilterBar {...props} />);
@@ -31,5 +34,63 @@ describe('FilterBar', () => {
     fireEvent.click(getByText('Tag A'));
     fireEvent.click(getByText('Tag B'));
     expect(props.onTagChange).toHaveBeenCalledWith([1, 2]);
+  });
+
+  beforeEach(() => {
+    (useAuth as jest.Mock).mockImplementation(() => ({
+      isAuthenticated: true,
+      user: mockUser,
+    }));
+  });
+
+  it('renders loading message when loading', () => {
+    (useQuery as jest.Mock).mockReturnValueOnce({
+      data: null,
+      isError: false,
+      error: null,
+      isLoading: true,
+    });
+
+    const { getByTestId } = render(<Dashboard />);
+
+    expect(getByTestId('loading-message')).toBeInTheDocument();
+  });
+
+  it('renders error message when there is an error', () => {
+    const mockError = {
+      message: 'An error occurred',
+      statusCode: 500,
+    };
+
+    (useQuery as jest.Mock).mockReturnValueOnce({
+      data: null,
+      isError: true,
+      error: mockError,
+      isLoading: false,
+    });
+
+    const { getByText } = render(<Dashboard />);
+
+    expect(getByText(mockError.message)).toBeInTheDocument();
+  });
+
+  it('renders message when there is no data', () => {
+    (useQuery as jest.Mock).mockReturnValueOnce({
+      data: {
+        data: [],
+        meta: {
+          pagination: {
+            pageCount: 0,
+          },
+        },
+      },
+      isError: false,
+      error: null,
+      isLoading: false,
+    });
+
+    const { getByTestId } = render(<Dashboard />);
+
+    expect(getByTestId('error-message')).toBeInTheDocument();
   });
 });
