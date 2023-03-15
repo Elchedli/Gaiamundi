743a95956522cabc7db0f31d09e6587dcfec1c92
elchedli <shidonosan@gmail.com>
2023-03-15 17:42:34 +0100

    refactor: datasetcolumnpicker refactor the whole file

--- packages/gaiamundi-web/src/components/PageCarto/Editor/PageCartoDataPanel/__test__/DatasetColumnPicker.test.tsx
+++ packages/gaiamundi-web/src/components/PageCarto/Editor/PageCartoDataPanel/__test__/DatasetColumnPicker.test.tsx
@@ -1,74 +1,89 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { mockDataFragmentsApiCollection } from 'utils/mocks/data';
 import DatasetColumnPicker from '../DatasetColumnPicker';
 
-const columns = mockDataFragmentsApiCollection.data[0].attributes.columns;
+const mockColumns = mockDataFragmentsApiCollection.data[0].attributes.columns;
 
 describe('DatasetColumnPicker', () => {
   window.HTMLElement.prototype.scrollIntoView = jest.fn();
 
-  it('renders with correct data', () => {
+  it('should renders with correct data', () => {
     const onChangeMock = jest.fn();
     const { getByText } = render(
-      <DatasetColumnPicker data={columns} onChange={onChangeMock} />
+      <DatasetColumnPicker data={mockColumns} onChange={onChangeMock} />
     );
-    columns.forEach((element) => {
+    mockColumns.forEach((element) => {
       expect(getByText(element.name)).toBeInTheDocument();
     });
   });
 
-  it('selects and deselects columns', () => {
+  it('should check if columns are selected or deselected', () => {
     const onChangeMock = jest.fn();
+
     const { container } = render(
-      <DatasetColumnPicker data={columns} onChange={onChangeMock} />
+      <DatasetColumnPicker data={mockColumns} onChange={onChangeMock} />
     );
+
     const columnTable = container.querySelectorAll(
       "div[role='grid']:nth-child(2) > div"
     );
     const column1 = columnTable[1];
     const column2 = columnTable[2];
-    column1.querySelector('input')?.click();
+
+    fireEvent.click(column1.querySelector('input') as HTMLElement);
+
     expect(column1.getAttribute('aria-selected')).toContain('true');
     expect(column2.getAttribute('aria-selected')).toContain('false');
-    column2.querySelector('input')?.click();
+
+    fireEvent.click(column2.querySelector('input') as HTMLElement);
+
     expect(column2.getAttribute('aria-selected')).toContain('true');
   });
 
-  it('filters rows based on search', () => {
+  it('should filter rows based on search of the column name', () => {
     const onChangeMock = jest.fn();
 
     const { container, getByText } = render(
-      <DatasetColumnPicker data={columns} onChange={onChangeMock} />
+      <DatasetColumnPicker data={mockColumns} onChange={onChangeMock} />
     );
 
     const input = container.querySelector('input:nth-child(2)') as HTMLElement;
-    const firstColumnName = columns[0].name;
+    const firstColumnName = mockColumns[0].name;
 
     const column1 = getByText(firstColumnName);
-    const column2 = getByText(columns[1].name);
+    const column2 = getByText(mockColumns[1].name);
 
     userEvent.type(input, firstColumnName);
+
     expect(column1).toBeInTheDocument();
     expect(column2).not.toBeInTheDocument();
   });
 
-  it('selects geoCode column', async () => {
+  it('should trigger onChange when geocode cell is selected', async () => {
     const onChange = jest.fn();
 
     const { container } = render(
-      <DatasetColumnPicker data={columns} onChange={onChange} />
+      <DatasetColumnPicker data={mockColumns} onChange={onChange} />
     );
 
-    container
-      .querySelectorAll("div[role='grid']:nth-child(2) > div")[1]
-      .querySelector('input')
-      ?.click();
-    container
-      .querySelectorAll("div[role='grid']:nth-child(2) > div")[1]
-      .querySelectorAll('input')[1]
-      .click();
+    const dataGridRow = container.querySelectorAll(
+      "div[role='grid']:nth-child(2) > div"
+    )[1];
+    const columnCheckbox = dataGridRow.querySelector('input[type=checkbox]');
 
-    expect(onChange).toHaveBeenCalledWith([columns[0]]);
+    expect(columnCheckbox).not.toBeNull();
+
+    fireEvent.click(columnCheckbox as Element);
+
+    await waitFor(() => {
+      const columnRadio = dataGridRow.querySelector('input[type=radio]');
+
+      expect(columnRadio).not.toBeNull();
+
+      fireEvent.click(columnRadio as Element);
+
+      expect(onChange).toHaveBeenCalledWith([mockColumns[0]]);
+    });
   });
 });
