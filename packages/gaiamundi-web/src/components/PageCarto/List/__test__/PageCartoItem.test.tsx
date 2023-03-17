c03f7dc7324124f23bae6f83c34e03a3f65e565a
Noure EL ASSIL <n.elassil95@gmail.com>
2023-03-17 14:35:26 +0100

    test: add test for badge component

--- packages/gaiamundi-web/src/components/PageCarto/List/__test__/PageCartoItem.test.tsx
+++ packages/gaiamundi-web/src/components/PageCarto/List/__test__/PageCartoItem.test.tsx
@@ -1,9 +1,9 @@
 import { render } from '@testing-library/react';
 import PageCartoItem from 'components/PageCarto/List/PageCartoItem';
+import { Badge } from 'components/Tags/Badge';
 import config from 'config';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { mockPageCartoData } from 'utils/mocks/data';
-
 describe('PageCartoItem', () => {
   it('renders PageCartoItem', () => {
     const { getByText, getByRole } = render(
@@ -29,4 +29,12 @@ describe('PageCartoItem', () => {
       `${config.API_URL}${mockPageCartoData.cover.formats.thumbnail.url}`
     );
   });
+});
+
+test('if we dont have herf, we dont have tage link', () => {
+  const { container } = render(<Badge />);
+  const herf = container.getAttribute('herf');
+  const link = container.querySelector('a');
+  expect(herf).toBe(null);
+  expect(link).not.toBeInTheDocument();
 });
