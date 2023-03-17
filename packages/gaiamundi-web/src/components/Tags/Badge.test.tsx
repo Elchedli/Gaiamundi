f972c7ba2c2ed74d0bd7a14bf02220f75f72b8f3
Noure EL ASSIL <n.elassil95@gmail.com>
2023-03-17 16:24:59 +0100

    test: add test for badge component

--- packages/gaiamundi-web/src/components/Tags/Badge.test.tsx
+++ packages/gaiamundi-web/src/components/Tags/Badge.test.tsx
@@ -1,11 +1,12 @@
 import { render } from '@testing-library/react';
 import { Badge } from 'components/Tags/Badge';
-test('if we dont have herf, we dont have tage link', () => {
-  const { container } = render(<Badge />);
-  const link = container.querySelector('a');
-  expect(link).not.toBeInTheDocument();
+describe('Badge', () => {
+  it('if we dont have herf, we dont have tage link', () => {
+    const { container } = render(<Badge />);
+    const link = container.querySelector('a');
+    expect(link).not.toBeInTheDocument();
+  });
 });
-
 test('if we have herf, we have tage link', () => {
   const { container } = render(<Badge href="#home" />);
   const link = container.querySelector('a');
