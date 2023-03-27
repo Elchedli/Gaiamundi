dbab25eae8aad5ae5990017c2bef23d66d8cd1f3
Ninon Tatu <ntatu.code@gmail.com>
2023-03-27 14:35:36 +0000

    Upload New File

---
+++ packages/gaiamundi-web/src/pages/TermsOfUse/TermsOfUse.tsx
@@ -0,0 +1,10 @@
+export const TermsOfUse = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eos
+      blanditiis sed ipsa inventore, assumenda consequatur adipisci. Earum
+      itaque exercitationem natus, quas explicabo corrupti voluptatem, ipsa,
+      accusantium perspiciatis illum quam.
+    </div>
+  );
+};
