dc855bb0ceb18a8b1d050fcee802aebbd9c3a173
Ninon Tatu <ntatu.code@gmail.com>
2023-03-29 12:03:15 +0000

    Replace TermsOfUse.tsx

--- packages/gaiamundi-web/src/pages/TermsOfUse/TermsOfUse.tsx
+++ packages/gaiamundi-web/src/pages/TermsOfUse/TermsOfUse.tsx
@@ -1,10 +1,146 @@
 export const TermsOfUse = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure eos
-      blanditiis sed ipsa inventore, assumenda consequatur adipisci. Earum
-      itaque exercitationem natus, quas explicabo corrupti voluptatem, ipsa,
-      accusantium perspiciatis illum quam.
+    <div className="mt-12">
+      <h2 className="text-3xl font-extrabold text-center text-transform: uppercase text-gray-900 mb-20">
+        Conditions générales d&apos;utilisation
+      </h2>
+      <h3 className="text-1xl font-extrabold 	text-transform: uppercase text-gray-900 mb-2">
+        Introduction
+      </h3>
+      <p className="text-justify text-left">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        repudiandae animi mollitia quibusdam vitae natus quidem. Ipsa pariatur
+        provident sapiente.Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae repudiandae animi mollitia quibusdam vitae natus quidem.
+        Ipsa pariatur provident sapiente. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Molestiae repudiandae animi mollitia
+        quibusdam vitae natus quidem. Ipsa pariatur provident sapiente.Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae
+        animi mollitia quibusdam vitae natus quidem. Ipsa pariatur provident
+        sapiente.
+      </p>
+      <h3 className="text-1xl font-extrabold 	text-transform: uppercase text-gray-900 mt-4 mb-2">
+        Utilisation du site
+      </h3>
+      <p className="text-justify text-left">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        repudiandae animi mollitia quibusdam vitae natus quidem. Ipsa pariatur
+        provident sapiente.Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae repudiandae animi mollitia quibusdam vitae natus quidem.
+        Ipsa pariatur provident sapiente. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Molestiae repudiandae animi mollitia
+        quibusdam vitae natus quidem. Ipsa pariatur provident sapiente.Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae
+        animi mollitia quibusdam vitae natus quidem. Ipsa pariatur provident
+        sapiente.
+      </p>
+      <h3 className="text-1xl font-extrabold 	text-transform: uppercase text-gray-900 mt-4  mb-2">
+        Conditions d&apos;accès et d&apos;utilisation
+      </h3>
+      <p className="text-justify text-left">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        repudiandae animi mollitia quibusdam vitae natus quidem. Ipsa pariatur
+        provident sapiente.Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae repudiandae animi mollitia quibusdam vitae natus quidem.
+        Ipsa pariatur provident sapiente. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Molestiae repudiandae animi mollitia
+        quibusdam vitae natus quidem. Ipsa pariatur provident sapiente.Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae
+        animi mollitia quibusdam vitae natus quidem. Ipsa pariatur provident
+        sapiente.
+      </p>
+      <h3 className="text-1xl font-extrabold 	text-transform: uppercase text-gray-900 mb-2">
+        Compte d&apos;utilisateurs
+      </h3>
+      <p className="text-justify text-left">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        repudiandae animi mollitia quibusdam vitae natus quidem. Ipsa pariatur
+        provident sapiente.Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae repudiandae animi mollitia quibusdam vitae natus quidem.
+        Ipsa pariatur provident sapiente. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Molestiae repudiandae animi mollitia
+        quibusdam vitae natus quidem. Ipsa pariatur provident sapiente.Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae
+        animi mollitia quibusdam vitae natus quidem. Ipsa pariatur provident
+        sapiente.
+      </p>
+      <h3 className="text-1xl font-extrabold 	text-transform: uppercase text-gray-900 mb-2">
+        Propriété intellectuelle
+      </h3>
+      <p className="text-justify text-left">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        repudiandae animi mollitia quibusdam vitae natus quidem. Ipsa pariatur
+        provident sapiente.Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae repudiandae animi mollitia quibusdam vitae natus quidem.
+        Ipsa pariatur provident sapiente. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Molestiae repudiandae animi mollitia
+        quibusdam vitae natus quidem. Ipsa pariatur provident sapiente.Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae
+        animi mollitia quibusdam vitae natus quidem. Ipsa pariatur provident
+        sapiente.
+      </p>
+      <h3 className="text-1xl font-extrabold 	text-transform: uppercase text-gray-900 mb-2">
+        Résiliation
+      </h3>
+      <p className="text-justify text-left">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        repudiandae animi mollitia quibusdam vitae natus quidem. Ipsa pariatur
+        provident sapiente.Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae repudiandae animi mollitia quibusdam vitae natus quidem.
+        Ipsa pariatur provident sapiente. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Molestiae repudiandae animi mollitia
+        quibusdam vitae natus quidem. Ipsa pariatur provident sapiente.Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae
+        animi mollitia quibusdam vitae natus quidem. Ipsa pariatur provident
+        sapiente.
+      </p>
+      <h3 className="text-1xl font-extrabold 	text-transform: uppercase text-gray-900 mb-2">
+        Confidentialité
+      </h3>
+      <p className="text-justify text-left">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero
+        necessitatibus cupiditate aperiam, placeat atque perspiciatis optio
+        laborum mollitia voluptas hic!Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Molestiae repudiandae animi mollitia quibusdam vitae
+        natus quidem. Ipsa pariatur provident sapiente.Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Molestiae repudiandae animi mollitia
+        quibusdam vitae natus quidem. Ipsa pariatur provident sapiente. Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae
+        animi mollitia quibusdam vitae natus quidem. Ipsa pariatur provident
+        sapiente.Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Molestiae repudiandae animi mollitia quibusdam vitae natus quidem. Ipsa
+        pariatur provident sapiente.
+      </p>
+      <h3 className="text-1xl font-extrabold 	text-transform: uppercase text-gray-900 mb-2">
+        Modifications
+      </h3>
+      <p className="text-justify text-left">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        repudiandae animi mollitia quibusdam vitae natus quidem. Ipsa pariatur
+        provident sapiente.Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae repudiandae animi mollitia quibusdam vitae natus quidem.
+        Ipsa pariatur provident sapiente. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Molestiae repudiandae animi mollitia
+        quibusdam vitae natus quidem. Ipsa pariatur provident sapiente.Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae
+        animi mollitia quibusdam vitae natus quidem. Ipsa pariatur provident
+        sapiente.
+      </p>
+      <h3 className="text-1xl font-extrabold 	text-transform: uppercase text-gray-900 mb-2">
+        Dispositions diverses
+      </h3>
+      <p className="text-justify text-left">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        repudiandae animi mollitia quibusdam vitae natus quidem. Ipsa pariatur
+        provident sapiente.Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae repudiandae animi mollitia quibusdam vitae natus quidem.
+        Ipsa pariatur provident sapiente. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Molestiae repudiandae animi mollitia
+        quibusdam vitae natus quidem. Ipsa pariatur provident sapiente.Lorem,
+        ipsum dolor sit amet consectetur adipisicing elit. Molestiae repudiandae
+        animi mollitia quibusdam vitae natus quidem. Ipsa pariatur provident
+        sapiente.
+      </p>
     </div>
   );
 };
