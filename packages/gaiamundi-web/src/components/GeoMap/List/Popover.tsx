ec72cc1432dcd985e9aa3c076ce976b753a13089
Noure EL ASSIL <n.elassil95@gmail.com>
2023-04-12 16:12:25 +0200

    feat: suggested change

--- packages/gaiamundi-web/src/components/GeoMap/List/Popover.tsx
+++ packages/gaiamundi-web/src/components/GeoMap/List/Popover.tsx
@@ -1,12 +1,12 @@
+import { InformationCircleIcon } from '@heroicons/react/24/solid';
 import { GeoProperty } from 'interfaces/geo-map';
 import { useState } from 'react';
-import { InformationCircleIcon } from '@heroicons/react/24/solid';
 interface GeoPropertyProps {
   properties: GeoProperty[];
 }
 export const Popover: React.FC<GeoPropertyProps> = ({ properties }) => {
   const [isVisible, setIsVisible] = useState(false);
-  function handleClik() {
+  function handleClick() {
     setIsVisible(!isVisible);
   }
 
@@ -16,7 +16,7 @@ export const Popover: React.FC<GeoPropertyProps> = ({ properties }) => {
         data-popover-target="popover-default"
         data-testid="button"
         type="button"
-        onClick={handleClik}
+        onClick={handleClick}
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 ml-65 absolute bottom-0 dark:focus:ring-blue-800"
       >
         <InformationCircleIcon width={24} height={24} />
