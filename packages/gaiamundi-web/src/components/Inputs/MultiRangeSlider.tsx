64867818193ba51fe829f6f56dfd26c7abdf2e3c
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2023-04-24 14:13:55 +0100

    feat: add slider controls

---
+++ packages/gaiamundi-web/src/components/Inputs/MultiRangeSlider.tsx
@@ -0,0 +1,108 @@
+import classnames from 'classnames';
+import {
+  ChangeEvent,
+  FC,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+interface MultiRangeSliderProps {
+  min: number;
+  max: number;
+  onChange: (range: { min: number; max: number }) => void;
+}
+
+const MultiRangeSlider: FC<MultiRangeSliderProps> = ({
+  min,
+  max,
+  onChange,
+}) => {
+  const [minVal, setMinVal] = useState(min);
+  const [maxVal, setMaxVal] = useState(max);
+  const minValRef = useRef<HTMLInputElement>(null);
+  const maxValRef = useRef<HTMLInputElement>(null);
+  const range = useRef<HTMLDivElement>(null);
+
+  const getPercent = useCallback(
+    (value: number) => Math.round(((value - min) / (max - min)) * 100),
+    [min, max]
+  );
+
+  const onSettingMinValue = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = Math.min(+event.target.value, maxVal - 1);
+    setMinVal(value);
+    event.target.value = value.toString();
+    onChange({ min: minVal, max: maxVal });
+  };
+
+  const onSettingMaxValue = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = Math.max(+event.target.value, minVal + 1);
+    setMaxVal(value);
+    event.target.value = value.toString();
+    onChange({ min: minVal, max: maxVal });
+  };
+
+  useEffect(() => {
+    if (maxValRef.current) {
+      const minPercent = getPercent(minVal);
+      const maxPercent = getPercent(+maxValRef.current.value);
+
+      if (range.current) {
+        range.current.style.left = `${minPercent}%`;
+        range.current.style.width = `${maxPercent - minPercent}%`;
+      }
+    }
+  }, [minVal, getPercent]);
+
+  useEffect(() => {
+    if (minValRef.current) {
+      const minPercent = getPercent(+minValRef.current.value);
+      const maxPercent = getPercent(maxVal);
+
+      if (range.current) {
+        range.current.style.width = `${maxPercent - minPercent}%`;
+      }
+    }
+  }, [maxVal, getPercent]);
+
+  return (
+    <div className="mb-4 py-4">
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={minVal}
+        ref={minValRef}
+        onChange={onSettingMinValue}
+        className={classnames('thumb z-30', {
+          'z-50': minVal > max - 100,
+        })}
+      />
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={maxVal}
+        ref={maxValRef}
+        onChange={onSettingMaxValue}
+        className="thumb z-40"
+      />
+
+      <div className="relative">
+        <div className="slider__track absolute w-full bg-gray-200 z-10 h-[12px] rounded-2xl"></div>
+        <div
+          ref={range}
+          className="slider__range z-20 absolute h-[12px] rounded-2xl"
+        ></div>
+        <div className=" mt-5 absolute text-sm text-[#6b7280] ">{minVal}</div>
+        <div className="-right-[4px] mt-5 absolute text-sm text-[#6b7280]">
+          {maxVal}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MultiRangeSlider;
---
+++ packages/gaiamundi-web/src/components/Inputs/Slider.tsx
@@ -0,0 +1,91 @@
+import classNames from 'classnames';
+import * as React from 'react';
+
+import { TextInput } from 'components/Inputs/TextInput';
+
+type Size = 'sm' | 'md' | 'lg';
+
+type SliderProps = {
+  defaultValue: number;
+  step?: string;
+  size?: Size;
+  minValue?: number;
+  maxValue?: number;
+  label?: string;
+  unit?: string;
+  onChange: (slider: number) => void;
+};
+const clamp = (value: number, min: number, max: number) =>
+  Math.min(Math.max(value, min), max);
+export const Slider: React.FC<SliderProps> = ({
+  defaultValue,
+  label,
+  onChange,
+  size = 'lg',
+  minValue = 0,
+  maxValue = Infinity,
+  step = 1,
+  unit = '',
+}) => {
+  const [value, setValue] = React.useState(
+    clamp(defaultValue, minValue, maxValue)
+  );
+
+  const parseNumber = (value: string) => {
+    const parsedValue = parseFloat(value);
+    return !isNaN(parsedValue) ? parsedValue : 0;
+  };
+
+  const updateSliderValue: React.FocusEventHandler<HTMLInputElement> = (e) => {
+    const newValue = parseNumber(e.target.value);
+    setValue(clamp(newValue, minValue, maxValue));
+    onChange(newValue);
+  };
+
+  const handleTextInput: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(parseNumber(e.target.value));
+  };
+
+  return (
+    <div className="flex flex-row">
+      <div className="w-full flex-grow">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+          {label}
+        </label>
+        <input
+          min={minValue}
+          max={maxValue}
+          type="range"
+          step={step}
+          value={value.toString()}
+          onChange={updateSliderValue}
+          className={classNames(
+            'mb-2 w-full bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700',
+            {
+              'h-1': size === 'sm',
+              'h-2': size === 'md',
+              'h-3': size === 'lg',
+            }
+          )}
+        />
+        <ul className="flex justify-between w-full">
+          <li className="flex justify-center relative">
+            {`${minValue.toFixed(0)}${unit}`}
+          </li>
+          <li className="flex justify-center relative">
+            {`${maxValue.toFixed(0)}${unit}`}
+          </li>
+        </ul>
+      </div>
+      <div className="w-2/12  ml-3">
+        <TextInput
+          sizing="sm"
+          className="w-full"
+          value={value.toString()}
+          onChange={handleTextInput}
+          onBlur={updateSliderValue}
+        />
+      </div>
+    </div>
+  );
+};
