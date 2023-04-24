f22e12e1dda2e6795f9c233520463017a8051d40
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2023-04-24 14:17:48 +0100

    feat: add chart icons

---
+++ packages/gaiamundi-web/src/components/Icons/Charts/BubbleChartIcon.tsx
@@ -0,0 +1,452 @@
+const BubbleChartIcon = (props: React.SVGProps<SVGSVGElement>): JSX.Element => {
+  return (
+    <svg viewBox="0 0 800 600" {...props}>
+      <g transform="translate(150, 100)">
+        <g className="ez-grid">
+          <g className="ez-grid-lines">
+            <line
+              transform="translate(0, 400.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 371.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 343.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 314.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 286.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 257.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 229.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 200.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 171.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 143.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 114.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 86.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 57.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 29.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(0, 0.5)"
+              x2="325"
+              y2="0"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+          </g>
+          <g className="ez-grid-lines">
+            <line
+              transform="translate(0.5, 0)"
+              x2="0"
+              y2="400"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(36.5, 0)"
+              x2="0"
+              y2="400"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(72.5, 0)"
+              x2="0"
+              y2="400"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(108.5, 0)"
+              x2="0"
+              y2="400"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(144.5, 0)"
+              x2="0"
+              y2="400"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(181.5, 0)"
+              x2="0"
+              y2="400"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(217.5, 0)"
+              x2="0"
+              y2="400"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(253.5, 0)"
+              x2="0"
+              y2="400"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(289.5, 0)"
+              x2="0"
+              y2="400"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+            <line
+              transform="translate(325.5, 0)"
+              x2="0"
+              y2="400"
+              stroke="rgba(168,168,168,1)"
+              className="ez-grid-line"
+            ></line>
+          </g>
+        </g>
+        <g className="ez-bubbles">
+          <circle
+            stroke="rgba(255, 85, 13, 0.69)"
+            fill="rgba(255, 85, 13, 0.69)"
+            r="9"
+            cx="0"
+            cy="349"
+            strokeWidth="1"
+            data-testid="ez-point"
+            className="ez-point"
+          ></circle>
+          <circle
+            stroke="rgba(255, 85, 13, 0.69)"
+            fill="rgba(255, 85, 13, 0.69)"
+            r="22"
+            cx="36"
+            cy="143"
+            strokeWidth="1"
+            data-testid="ez-point"
+            className="ez-point"
+          ></circle>
+          <circle
+            stroke="rgba(255, 85, 13, 0.69)"
+            fill="rgba(255, 85, 13, 0.69)"
+            r="16"
+            cx="72"
+            cy="234"
+            strokeWidth="1"
+            data-testid="ez-point"
+            className="ez-point"
+          ></circle>
+          <circle
+            stroke="rgba(255, 85, 13, 0.69)"
+            fill="rgba(255, 85, 13, 0.69)"
+            r="17"
+            cx="108"
+            cy="229"
+            strokeWidth="1"
+            data-testid="ez-point"
+            className="ez-point"
+          ></circle>
+          <circle
+            stroke="rgba(255, 85, 13, 0.69)"
+            fill="rgba(255, 85, 13, 0.69)"
+            r="24"
+            cx="144"
+            cy="114"
+            strokeWidth="1"
+            data-testid="ez-point"
+            className="ez-point"
+          ></circle>
+          <circle
+            stroke="rgba(255, 85, 13, 0.69)"
+            fill="rgba(255, 85, 13, 0.69)"
+            r="13"
+            cx="181"
+            cy="291"
+            strokeWidth="1"
+            data-testid="ez-point"
+            className="ez-point"
+          ></circle>
+          <circle
+            stroke="rgba(255, 85, 13, 0.69)"
+            fill="rgba(255, 85, 13, 0.69)"
+            r="15"
+            cx="217"
+            cy="257"
+            strokeWidth="1"
+            data-testid="ez-point"
+            className="ez-point"
+          ></circle>
+          <circle
+            stroke="rgba(255, 85, 13, 0.69)"
+            fill="rgba(255, 85, 13, 0.69)"
+            r="17"
+            cx="253"
+            cy="229"
+            strokeWidth="1"
+            data-testid="ez-point"
+            className="ez-point"
+          ></circle>
+          <circle
+            stroke="rgba(255, 85, 13, 0.69)"
+            fill="rgba(255, 85, 13, 0.69)"
+            r="7"
+            cx="289"
+            cy="389"
+            strokeWidth="1"
+            data-testid="ez-point"
+            className="ez-point"
+          ></circle>
+          <circle
+            stroke="rgba(255, 85, 13, 0.69)"
+            fill="rgba(255, 85, 13, 0.69)"
+            r="29"
+            cx="325"
+            cy="29"
+            strokeWidth="1"
+            data-testid="ez-point"
+            className="ez-point"
+          ></circle>
+        </g>
+        <g
+          textAnchor="middle"
+          transform="translate(0, 400)"
+          className="ez-axis"
+        >
+          <path d="M0.5,6V-6V-0.5H325.5V6V-6" className="ez-axis-path"></path>
+          <g transform="translate(0.5, 0)" className="ez-axis-tick">
+            <line x2="0" y2="6" className="ez-axis-tick-line"></line>
+            <text y="9" dy="0.71em" className="ez-axis-tick-text">
+              1
+            </text>
+          </g>
+          <g transform="translate(36.5, 0)" className="ez-axis-tick">
+            <line x2="0" y2="6" className="ez-axis-tick-line"></line>
+            <text y="9" dy="0.71em" className="ez-axis-tick-text">
+              2
+            </text>
+          </g>
+          <g transform="translate(72.5, 0)" className="ez-axis-tick">
+            <line x2="0" y2="6" className="ez-axis-tick-line"></line>
+            <text y="9" dy="0.71em" className="ez-axis-tick-text">
+              3
+            </text>
+          </g>
+          <g transform="translate(108.5, 0)" className="ez-axis-tick">
+            <line x2="0" y2="6" className="ez-axis-tick-line"></line>
+            <text y="9" dy="0.71em" className="ez-axis-tick-text">
+              4
+            </text>
+          </g>
+          <g transform="translate(144.5, 0)" className="ez-axis-tick">
+            <line x2="0" y2="6" className="ez-axis-tick-line"></line>
+            <text y="9" dy="0.71em" className="ez-axis-tick-text">
+              5
+            </text>
+          </g>
+          <g transform="translate(181.5, 0)" className="ez-axis-tick">
+            <line x2="0" y2="6" className="ez-axis-tick-line"></line>
+            <text y="9" dy="0.71em" className="ez-axis-tick-text">
+              6
+            </text>
+          </g>
+          <g transform="translate(217.5, 0)" className="ez-axis-tick">
+            <line x2="0" y2="6" className="ez-axis-tick-line"></line>
+            <text y="9" dy="0.71em" className="ez-axis-tick-text">
+              7
+            </text>
+          </g>
+          <g transform="translate(253.5, 0)" className="ez-axis-tick">
+            <line x2="0" y2="6" className="ez-axis-tick-line"></line>
+            <text y="9" dy="0.71em" className="ez-axis-tick-text">
+              8
+            </text>
+          </g>
+          <g transform="translate(289.5, 0)" className="ez-axis-tick">
+            <line x2="0" y2="6" className="ez-axis-tick-line"></line>
+            <text y="9" dy="0.71em" className="ez-axis-tick-text">
+              9
+            </text>
+          </g>
+          <g transform="translate(325.5, 0)" className="ez-axis-tick">
+            <line x2="0" y2="6" className="ez-axis-tick-line"></line>
+            <text y="9" dy="0.71em" className="ez-axis-tick-text">
+              10
+            </text>
+          </g>
+        </g>
+        <g textAnchor="end" transform="translate(0, 0)" className="ez-axis">
+          <path d="M6,400.5H-6H-0.5V0.5H6,-6" className="ez-axis-path"></path>
+          <g transform="translate(0, 400.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              0
+            </text>
+          </g>
+          <g transform="translate(0, 371.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              5
+            </text>
+          </g>
+          <g transform="translate(0, 343.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              10
+            </text>
+          </g>
+          <g transform="translate(0, 314.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              15
+            </text>
+          </g>
+          <g transform="translate(0, 286.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              20
+            </text>
+          </g>
+          <g transform="translate(0, 257.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              25
+            </text>
+          </g>
+          <g transform="translate(0, 229.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              30
+            </text>
+          </g>
+          <g transform="translate(0, 200.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              35
+            </text>
+          </g>
+          <g transform="translate(0, 171.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              40
+            </text>
+          </g>
+          <g transform="translate(0, 143.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              45
+            </text>
+          </g>
+          <g transform="translate(0, 114.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              50
+            </text>
+          </g>
+          <g transform="translate(0, 86.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              55
+            </text>
+          </g>
+          <g transform="translate(0, 57.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              60
+            </text>
+          </g>
+          <g transform="translate(0, 29.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              65
+            </text>
+          </g>
+          <g transform="translate(0, 0.5)" className="ez-axis-tick">
+            <line x2="-6" y2="0" className="ez-axis-tick-line"></line>
+            <text x="-9" dy="0.32em" className="ez-axis-tick-text">
+              70
+            </text>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+export default BubbleChartIcon;
