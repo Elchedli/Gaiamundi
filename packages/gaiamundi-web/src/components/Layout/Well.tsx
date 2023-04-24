cebe718aeef08a310a70a8edec4a5a842db06962
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2023-04-24 14:20:22 +0100

    feat: add well component

---
+++ packages/gaiamundi-web/src/components/Layout/Well.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+interface WellProps {
+  title?: string;
+  Icon?: React.FC<React.ComponentProps<'svg'>>;
+  children: React.ReactNode;
+}
+
+const Well = ({ title, Icon, children }: WellProps): JSX.Element => {
+  return (
+    <div className="relative rounded-md border border-gray-200 bg-white shadow-md p-5 m-2">
+      {title && (
+        <h2 className="mb-3 text-xl font-semibold font-medium text-gray-900 title-font">
+          {Icon && <Icon className="h-5 w-5 text-blue-500 inline" />} {title}
+        </h2>
+      )}
+      <div className="shadow-none">{children}</div>
+    </div>
+  );
+};
+
+export default Well;
