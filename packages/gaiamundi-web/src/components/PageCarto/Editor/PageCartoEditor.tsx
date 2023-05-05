d8616f4fd9a8e3df39c910d8ad27ce8ebc5ecd7c
noure <n.elassil95@gmail.com>
2023-05-05 10:06:17 +0200

    feat: resizablepanel

--- packages/gaiamundi-web/src/components/PageCarto/Editor/PageCartoEditor.tsx
+++ packages/gaiamundi-web/src/components/PageCarto/Editor/PageCartoEditor.tsx
@@ -1,16 +1,50 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 import { PageCartoMap } from './PageCartoMap';
 import { PageCartoPanels } from './PageCartoPanels';
 
 export const PageCartoEditor: FC = () => {
+  const panelRef = useRef({} as HTMLDivElement);
+  let startX = 0;
+  let panelWidth = 0;
+  const handleMouseDown = (event: { pageX: number }) => {
+    startX = event.pageX;
+    panelWidth = panelRef.current?.offsetWidth;
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+  };
+  const handleMouseMove = (event: { pageX: number }) => {
+    const diffX = event.pageX - startX;
+    const newWidth = panelWidth - diffX;
+    if (newWidth >= 0) {
+      panelRef.current.style.width = newWidth + 'px';
+    }
+  };
+  const handleMouseUp = () => {
+    document.removeEventListener('mousemove', handleMouseMove);
+    document.removeEventListener('mouseup', handleMouseUp);
+  };
   return (
-    <div className="grid grid-cols-3 gap-4 h-full">
-      <div className="col-span-2">
-        <PageCartoMap />
+    <>
+      <div
+        data-testid="page-panels"
+        ref={panelRef}
+        style={{
+          position: 'absolute',
+          right: '0',
+          maxWidth: '1390px',
+        }}
+        onMouseDown={handleMouseDown}
+      >
+        <div className="grid grid-cols-3 h-full">
+          <div className="col-span-2 h-full">
+            <PageCartoMap />
+          </div>
+
+          <div className="col-span cursor-col-resize h-5/6">
+            <PageCartoPanels />
+          </div>
+        </div>
       </div>
-      <div className="col-span">
-        <PageCartoPanels />
-      </div>
-    </div>
+    </>
   );
 };
