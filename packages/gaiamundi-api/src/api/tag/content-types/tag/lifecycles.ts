44975d749267d8e70a858c06d41797e0a7ebcd1e
elchedli <shidonosan@gmail.com>
2023-05-18 10:48:57 +0100

    fix: tags are counted correctly with user pageCartos in mind

--- packages/gaiamundi-api/src/api/tag/content-types/tag/lifecycles.ts
+++ packages/gaiamundi-api/src/api/tag/content-types/tag/lifecycles.ts
@@ -1,8 +1,12 @@
 module.exports = {
-  afterFindMany: async (event) => {
-    const { result } = event;
+  afterFindMany: async ({ result, params }) => {
+    const userid = params.where.page_cartos.owner.id.$eq;
     result.forEach((tag) => {
-      const count = tag.page_cartos ? tag.page_cartos.length : 0;
+      // console.log(tag.page_cartos[0].owner.id);
+      const count = tag.page_cartos
+        ? tag.page_cartos.filter((pageCarto) => pageCarto.owner.id == userid)
+            .length
+        : 0;
       delete tag.page_cartos;
       tag.count = count;
     });
