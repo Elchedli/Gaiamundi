96d40107ba97eb23229223a6d85c97d052aa868d
AndyMatloub <andy.matloub@hotmail.com>
2023-05-19 13:18:36 +0200

    fix: minor changes

--- packages/gaiamundi-web/src/components/PageCarto/ReadOnly/PageCartoContent.tsx
+++ packages/gaiamundi-web/src/components/PageCarto/ReadOnly/PageCartoContent.tsx
@@ -1,10 +1,9 @@
 import { FC } from 'react';
 
 export const PageCartoContent: FC = () => {
-  const htmlContent = '<TextEditor />';
   return (
     <div>
-      <div dangerouslySetInnerHTML={{ __html: htmlContent }}></div>
+      <div dangerouslySetInnerHTML={{ __html: '<TextEditor />' }}></div>
     </div>
   );
 };
--- packages/gaiamundi-web/src/components/PageCarto/ReadOnly/PageCartoReadOnly.tsx
+++ packages/gaiamundi-web/src/components/PageCarto/ReadOnly/PageCartoReadOnly.tsx
@@ -1,17 +1,15 @@
 import { FC } from 'react';
 import { PageCartoMap } from '../Editor/PageCartoMap';
-import { PageCartoPanels } from '../Editor/PageCartoPanels';
 import { PageCartoContent } from './PageCartoContent';
 
 export const PageCartoReadOnly: FC = () => {
-  const canEdit = false;
   return (
     <div className="grid grid-cols-3 gap-4 h-full">
       <div className="col-span-2">
-        <PageCartoMap canEdit={canEdit} />
+        <PageCartoMap canEdit={false} />
       </div>
       <div className="col-span">
-        {canEdit ? <PageCartoPanels /> : <PageCartoContent />}
+        <PageCartoContent />
       </div>
     </div>
   );
