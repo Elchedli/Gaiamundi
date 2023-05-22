315f3e5e8421db9acb044e2a9bdaa5702af922ef
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2023-05-22 11:54:19 +0100

    feat: add snapshot + wysiwyg custom btn

---
+++ packages/gaiamundi-web/src/components/PageCarto/Editor/PageCartoHyperTextPanel/SnapshotButton.tsx
@@ -0,0 +1,37 @@
+import { TOGGLE_LINK_COMMAND } from '@lexical/link';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { usePageCarto } from 'hooks/usePageCarto';
+import { useSnapshot } from 'hooks/useSnapshot';
+
+const SnapshotButton = () => {
+  const { pageCartoId } = usePageCarto();
+  const { addSnapshot } = useSnapshot();
+  const [editor] = useLexicalComposerContext();
+
+  const handleClick = async () => {
+    try {
+      const snapshot = await addSnapshot();
+      let link = `/page-carto/${pageCartoId}/snapshot/${snapshot.data.id}`;
+      if (snapshot.data.geoCode) {
+        link += `/selected/${snapshot.data.geoCode}`;
+      }
+      editor.dispatchCommand(TOGGLE_LINK_COMMAND, link);
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.warn('Unable to create snapshot');
+    }
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className={'toolbar-item spaced '}
+      title={`Snapshot`}
+      type="button"
+    >
+      <i className="icon plus" /> <span className="text">Snapshot</span>
+    </button>
+  );
+};
+
+export default SnapshotButton;
