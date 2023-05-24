17772e03a51825885e5b1fa701ec46f5df995115
AndyMatloub <andy.matloub@hotmail.com>
2023-05-24 14:18:49 +0200

    feat: change in PageCartoReadOnly

--- packages/gaiamundi-web/src/components/PageCarto/ReadOnly/PageCartoReadOnly.tsx
+++ packages/gaiamundi-web/src/components/PageCarto/ReadOnly/PageCartoReadOnly.tsx
@@ -1,15 +1,19 @@
 import { FC } from 'react';
+import { PageCartoChartPanel } from '../Editor/PageCartoChartPanel/PageCartoChartPanel';
 import { PageCartoMap } from '../Editor/PageCartoMap';
 import { PageCartoContent } from './PageCartoContent';
 
 export const PageCartoReadOnly: FC = () => {
   return (
-    <div className="grid grid-cols-3 gap-4 h-full">
-      <div className="col-span-2">
+    <div className="grid grid-cols-3 h-full grid-rows-2">
+      <div className="col-span-2 row-span-2 p-2">
         <PageCartoMap canEdit={false} />
       </div>
-      <div className="col-span">
+      <div className="col-span row-span-1 p-2">
         <PageCartoContent />
+      </div>
+      <div className="col-span row-span-1 p-2">
+        <PageCartoChartPanel />
       </div>
     </div>
   );
