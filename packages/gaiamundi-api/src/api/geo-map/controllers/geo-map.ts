0dd89a919fefc9d13620df7b9707245e3e18ee7f
sihamouda <anis.c.hamouda@gmail.com>
2023-11-02 17:29:11 +0100

    fix: replaced svg generation library in api

--- packages/gaiamundi-api/src/api/geo-map/controllers/geo-map.ts
+++ packages/gaiamundi-api/src/api/geo-map/controllers/geo-map.ts
@@ -4,7 +4,8 @@
 
 import { factories } from "@strapi/strapi";
 import fs from "fs";
-import geojson2svg from "geojson-to-svg";
+import { GeoJSON2SVG } from "geojson2svg";
+import parsePath from "parse-svg-path";
 import path from "path";
 
 export default factories.createCoreController(
@@ -17,7 +18,6 @@ export default factories.createCoreController(
         "plugin::upload.file",
         fileId
       );
-      console.log(JSON.stringify(entry), entry.hash);
       // compute svg path
       const svgFilename = `${entry.hash}.svg`;
       const svgPath = path.join(
@@ -34,9 +34,33 @@ export default factories.createCoreController(
           `${entry.hash}${entry.ext}`
         );
         const data = require(geoJsonPath);
-        svg = geojson2svg()
-          .projection(([x, y]) => [x, -y])
-          .render(data);
+
+        const converterSVG = new GeoJSON2SVG({
+          attributes: {
+            style: "stroke:black; fill: #FFFFFF;stroke-width:0.5px",
+          },
+        });
+
+        const converterPath = new GeoJSON2SVG({
+          output: "path",
+        });
+
+        const dataSVG = converterSVG.convert(data);
+        const dataPaths = converterPath.convert(data);
+
+        let width = 0;
+        let height = 0;
+
+        for (const path of dataPaths) {
+          const points = parsePath(path);
+          points.pop();
+          const [maxX, maxY] = points.reduce(([xMax, yMax], [,xCurrent,yCurrent])=> [Math.max(xMax,xCurrent),Math.max(yMax,yCurrent)],[0,0])
+          if (maxY > height) height = maxY;
+          if (maxX > width) width = maxX;
+        }
+        const dataSVGElements = dataSVG.join("\n")
+        svg = `<?xml version="1.0" standalone="no"?>\n<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}">\n${dataSVGElements}\n</svg>`;
+
         fs.writeFileSync(svgPath, svg);
       } else {
         // get existing svg thumbnail
