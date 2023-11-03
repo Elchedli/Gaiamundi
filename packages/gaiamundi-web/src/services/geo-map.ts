10d9db8a02f2f5613f1a00fde71dca941410eca2
sihamouda <anis.c.hamouda@gmail.com>
2023-11-03 16:51:47 +0100

    fix: convert geojson to json

--- packages/gaiamundi-web/src/services/geo-map.ts
+++ packages/gaiamundi-web/src/services/geo-map.ts
@@ -3,13 +3,25 @@ import { ApiData, ApiErrorResponse } from 'interfaces/api';
 import { UploadedFile } from 'interfaces/file';
 import { GeoMapStub } from 'interfaces/geo-map';
 import { ContentType, strapi } from './strapi';
+import { validateGeoJsonFile } from 'utils/file';
 
 export const createGeoMap = async (data: GeoMapStub) => {
   return await strapi.create<GeoMapStub>(ContentType.GEO_MAPS, data);
 };
 
+const createGeoJson = async (file: File) => {
+  if (file.type === 'application/json') {
+    return file;
+  }
+  const GeoJsonFileContent = await validateGeoJsonFile(file);
+  return new File([JSON.stringify(GeoJsonFileContent)], `${file.name}.json`, {
+    type: 'application/json',
+  });
+};
+
 export const uploadGeoJson = async (file: File) => {
-  return await strapi.uploadFile(file, 'api::geo-map.geo-map');
+  const fileToUpload = await createGeoJson(file);
+  return await strapi.uploadFile(fileToUpload, 'api::geo-map.geo-map');
 };
 
 export const getGeoJson = async (geoJSON: ApiData<UploadedFile>) => {
